ff0b49374ae1d98e23c358c30ffb492299a95911
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2024-03-27 10:38:09 -0300

    is isomorphic o(n_

--- interview/hashmap/isomorphic_strings.py
+++ interview/hashmap/isomorphic_strings.py
@@ -1,17 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         s_dict = {}
-        teste = []
-        
+        t_dict = {}
+
         for i in range(len(s)):
-            if s[i] not in s_dict:
-                s_dict[s[i]] = [t[i], 1]  # If the character is not in the dictionary, add it with count 1
-            else:
-                s_dict[s[i]][1] += 1  # If it's already in the dictionary, increase the count
-        
-        for i in range(len(t)):
-            if s_dict.get(s[i]) and s_dict[s[i]][1] >= 1:
-                teste.append(s_dict[s[i]][0])
-                s_dict[s[i]][1] -= 1  # Reduce the count of occurrences
-            
-        return teste == list(t)
+            if s[i] not in s_dict and t[i] not in t_dict:
+                s_dict[s[i]] = t[i]
+                t_dict[t[i]] = s[i]
+            elif s_dict.get(s[i]) != t[i] or t_dict.get(t[i]) != s[i]:
+                return False
+
+        return True
