9f13ea213fac229fc5de5f02b48da9fa28ba11ee
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2024-03-27 19:09:29 -0300

    same tree

---
+++ daily/dfs.py
@@ -0,0 +1,24 @@
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node)
+            visited.add(node)
+            for neighbour in graph[node]:
+                if neighbour not in visited:
+                    stack.append(neighbour)
+
+# example usage:
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E'],
+}
+
+dfs(graph, 'A')
