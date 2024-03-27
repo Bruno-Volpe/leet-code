5d0045d76c769796449a939f5e83e15077a3882d
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2024-03-27 10:17:19 -0300

    isomorfic working

--- interview/hashmap/isomorphic_strings.py
+++ interview/hashmap/isomorphic_strings.py
@@ -4,10 +4,14 @@ class Solution:
         teste = []
         
         for i in range(len(s)):
-            s_dict[s[i]] = t[i]
+            if s[i] not in s_dict:
+                s_dict[s[i]] = [t[i], 1]  # If the character is not in the dictionary, add it with count 1
+            else:
+                s_dict[s[i]][1] += 1  # If it's already in the dictionary, increase the count
         
         for i in range(len(t)):
-            teste.append(s_dict[s[i]])
+            if s_dict.get(s[i]) and s_dict[s[i]][1] >= 1:
+                teste.append(s_dict[s[i]][0])
+                s_dict[s[i]][1] -= 1  # Reduce the count of occurrences
             
         return teste == list(t)
-        
