20f947e6e99204d476bac5e64444f1b09815aa06
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2024-04-03 11:04:59 -0300

    bhacktrack explain

--- daily/word_search.py
+++ daily/word_search.py
@@ -3,18 +3,21 @@ class Solution:
         # BACKTRACKING
         # Time: O(N * 4^L) where N is the number of cells in the board and L is the length of the word
         # Space: O(L) where L is the length of the word
-        def backtrack(r, c, i):
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
+        
+        def dfs(r, c, i):
             if i == len(word):
                 return True
-            if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]) or board[r][c] != word[i]:
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or board[r][c] != word[i] or (r, c) in path:
                 return False
-            board[r][c] = '#'
-            res = backtrack(r + 1, c, i + 1) or backtrack(r - 1, c, i + 1) or backtrack(r, c + 1, i + 1) or backtrack(r, c - 1, i + 1)
-            board[r][c] = word[i]
+            path.add((r, c))
+            res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
+            path.remove((r, c))
             return res
         
-        for r in range(len(board)):
-            for c in range(len(board[0])):
-                if board[r][c] == word[0] and backtrack(r, c, 0):
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
                     return True
         return False
